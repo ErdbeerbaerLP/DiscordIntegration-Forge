4e8758c0bf61a9ca95666ad02e11373a69b2d727
Erdbeerbaer <Erdbeerbaer@LAPTOP-S0IE2HLV.fritz.box>
2019-07-03 10:58:44 +0200

    Update version number

--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -52,7 +52,7 @@ public class DiscordIntegration {
 	/**
 	 * Mod version
 	 */
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	/**
 	 * Modid
 	 */
