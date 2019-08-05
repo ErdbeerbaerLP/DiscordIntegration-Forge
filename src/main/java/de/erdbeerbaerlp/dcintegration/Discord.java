06d9958dc22606dfc469620ea2a2917afc1695e0
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-08-05 19:51:10 +0200

    Finally FIX that bug, 2 mintue shutdown/restart message fixed!!!

--- src/main/java/de/erdbeerbaerlp/dcintegration/Discord.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/Discord.java
@@ -112,7 +112,8 @@ public class Discord implements EventListener{
 		public void run() {
 			while(true) {
 				final long timeLeft = TimeUnit.MILLISECONDS.toSeconds(FTBUtilitiesUniverseData.shutdownTime-Instant.now().toEpochMilli());
-				if (timeLeft == TimeUnit.SECONDS.toMinutes(2))
+				System.out.println(timeLeft);
+				if (timeLeft == 120)
 					sendMessage(Configuration.FTB_UTILITIES.SHUTDOWN_MSG_2MINUTES, Configuration.FTB_UTILITIES.FTB_AVATAR_ICON, "FTB Utilities", null);
 				else if(timeLeft == 10) {
 					sendMessage(Configuration.FTB_UTILITIES.SHUTDOWN_MSG_10SECONDS, Configuration.FTB_UTILITIES.FTB_AVATAR_ICON, "FTB Utilities", null);
--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -44,7 +44,7 @@ public class DiscordIntegration {
 	/**
 	 * Mod version
 	 */
-	public static final String VERSION = "1.0.7";
+	public static final String VERSION = "1.0.8";
 	/**
 	 * Modid
 	 */
