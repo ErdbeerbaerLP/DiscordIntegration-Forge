7a68e697f654e25bc687b351270c2fe9f75f517c
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-09-15 14:18:12 +0200

    Fix version number

--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -46,7 +46,7 @@ public class DiscordIntegration {
 	/**
 	 * Mod version
 	 */
-	public static final String VERSION = "1.0.10";
+    public static final String VERSION = "1.0.11";
 	/**
 	 * Modid
 	 */
