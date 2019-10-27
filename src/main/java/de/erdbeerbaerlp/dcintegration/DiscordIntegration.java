9d3d3da0809ab2b8c3069d7a9e4130d460458575
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-10-27 13:29:33 +0100

    Small bugfix

--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -55,7 +55,7 @@ public class DiscordIntegration
     /**
      * Mod version
      */
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     /**
      * Modid
      */
@@ -304,9 +304,10 @@ public class DiscordIntegration
             for (String s : ev.getParameters()) {
                 msg = msg + s + " ";
             }
-            if (ev.getSender() instanceof DedicatedServer) discord_instance.sendMessage("*" + msg + "*");
+            if (ev.getCommand().getName().equals("me")) msg = "*" + msg.trim() + "*";
+            if (ev.getSender() instanceof DedicatedServer) discord_instance.sendMessage(msg);
             else if (ev.getSender() instanceof EntityPlayer || ev.getSender() instanceof FakePlayer) discord_instance.sendMessage(formatPlayerName((EntityPlayer) ev.getSender()),
-                                                                                                                                  ev.getSender().getCommandSenderEntity().getUniqueID().toString(), "*" + msg + "*");
+                                                                                                                                  ev.getSender().getCommandSenderEntity().getUniqueID().toString(), msg);
         }
     }
     
