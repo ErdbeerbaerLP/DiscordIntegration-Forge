883b1d583fc31aa60e245f1fe0ae3f6a6b0316cc
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-12-21 22:49:03 +0100

    Update version number

--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -56,7 +56,7 @@ public class DiscordIntegration
     /**
      * Mod version
      */
-    public static final String VERSION = "1.1.8";
+    public static final String VERSION = "1.1.10";
     /**
      * Modid
      */
