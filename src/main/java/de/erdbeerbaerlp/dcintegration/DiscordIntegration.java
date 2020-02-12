bc88def15d64169082e091f8d9a6409431a89b85
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2020-02-12 17:33:57 +0100

    Allow removing of server starting message

--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -51,7 +51,7 @@ public class DiscordIntegration {
     /**
      * Mod version
      */
-    public static final String VERSION = "1.1.13";
+    public static final String VERSION = "1.1.14";
     /**
      * Modid
      */
