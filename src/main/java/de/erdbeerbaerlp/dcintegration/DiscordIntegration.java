14e7a93dd672cff17fcbc87568cf6e4473fd77f9
ErdbeerbaerLP <56-ErdbeerbaerLP@users.noreply.git.ender.io>
2020-04-30 21:02:40 +0200

    Fix #54

--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -54,7 +54,7 @@ public class DiscordIntegration {
     /**
      * Mod version
      */
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.2.2";
     /**
      * Modid
      */
