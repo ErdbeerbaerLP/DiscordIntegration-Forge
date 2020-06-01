7d84faa954e271b57bb37c4cd91b0178195e6e66
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2020-06-01 09:32:32 +0200

    Fix possible crashes with update checker and fix forgetting to increment version number :/.  Closes #72
    Signed-off-by: ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>

--- src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/DiscordIntegration.java
@@ -52,7 +52,7 @@ public class DiscordIntegration {
     /**
      * Mod version
      */
-    public static final String VERSION = "1.2.4";
+    public static final String VERSION = "1.2.6";
     /**
      * Modid
      */
