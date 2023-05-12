4db450c635ec845d8182736e4c87f0d9af8c013c
zhongfu <me@zhongfu.li>
2023-05-12 18:31:21 +0800

    feat: add role color/hover/click formatting to reaction messages

--- src/main/java/de/erdbeerbaerlp/dcintegration/forge/util/ForgeServerInterface.java
+++ src/main/java/de/erdbeerbaerlp/dcintegration/forge/util/ForgeServerInterface.java
@@ -5,7 +5,13 @@ import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dcshadow.dev.vankka.mcdiscordreserializer.minecraft.MinecraftSerializer;
 import dcshadow.net.kyori.adventure.text.Component;
+import dcshadow.net.kyori.adventure.text.TextReplacementConfig;
+import dcshadow.net.kyori.adventure.text.event.ClickEvent;
+import dcshadow.net.kyori.adventure.text.event.HoverEvent;
+import dcshadow.net.kyori.adventure.text.format.Style;
+import dcshadow.net.kyori.adventure.text.format.TextColor;
 import dcshadow.net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import dcshadow.net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import de.erdbeerbaerlp.dcintegration.common.DiscordEventListener;
 import de.erdbeerbaerlp.dcintegration.common.storage.Configuration;
 import de.erdbeerbaerlp.dcintegration.common.storage.Localization;
@@ -76,15 +82,28 @@ public class ForgeServerInterface implements ServerInterface {
         final List<ServerPlayer> l = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers();
         for (final ServerPlayer p : l) {
             if (p.getUUID().equals(targetUUID) && !Variables.discord_instance.ignoringPlayers.contains(p.getUUID()) && !PlayerLinkController.getSettings(null, p.getUUID()).ignoreDiscordChatIngame && !PlayerLinkController.getSettings(null, p.getUUID()).ignoreReactions) {
+                final String emote = ":" + reactionEmote.getName() + ":";
 
-                final String emote =  ":" + reactionEmote.getName() + ":";
-                String outMsg = Localization.instance().reactionMessage.replace("%name%", member.getEffectiveName()).replace("%name2%", member.getUser().getAsTag()).replace("%emote%", emote);
+                Style.Builder memberStyle = Style.style();
+                if (Configuration.instance().messages.discordRoleColorIngame)
+                    memberStyle = memberStyle.color(TextColor.color(member.getColorRaw()));
+
+                final Component user = Component.text(member.getEffectiveName()).style(memberStyle
+                        .clickEvent(ClickEvent.suggestCommand("<@" + member.getId() + ">"))
+                        .hoverEvent(HoverEvent.showText(Component.text(Localization.instance().discordUserHover.replace("%user#tag%", member.getUser().getAsTag()).replace("%user%", member.getEffectiveName()).replace("%id%", member.getUser().getId())))));
+                final TextReplacementConfig userReplacer = ComponentUtils.replaceLiteral("%user%", user);
+                final TextReplacementConfig emoteReplacer = ComponentUtils.replaceLiteral("%emote%", emote);
+
+                final Component out = LegacyComponentSerializer.legacySection().deserialize(Localization.instance().reactionMessage)
+                        .replaceText(userReplacer).replaceText(emoteReplacer);
+
                 if (Localization.instance().reactionMessage.contains("%msg%"))
                     retrieveMessage.submit().thenAccept((m) -> {
-                        String outMsg2 = outMsg.replace("%msg%", m.getContentDisplay());
-                        sendReactionMCMessage(p, ForgeMessageUtils.formatEmoteMessage(m.getMentions().getCustomEmojis(), outMsg2));
+                        final String msg = ForgeMessageUtils.formatEmoteMessage(m.getMentions().getCustomEmojis(), m.getContentDisplay());
+                        final TextReplacementConfig msgReplacer = ComponentUtils.replaceLiteral("%msg%", msg);
+                        sendReactionMCMessage(p, out.replaceText(msgReplacer));
                     });
-                else sendReactionMCMessage(p, outMsg);
+                else sendReactionMCMessage(p, out);
             }
         }
     }
@@ -130,8 +149,7 @@ public class ForgeServerInterface implements ServerInterface {
         return "Forge";
     }
 
-    private void sendReactionMCMessage(ServerPlayer target, String msg) {
-        final Component msgComp = MinecraftSerializer.INSTANCE.serialize(msg.replace("\n", "\\n"), DiscordEventListener.mcSerializerOptions);
+    private void sendReactionMCMessage(ServerPlayer target, Component msgComp) {
         final String jsonComp = GsonComponentSerializer.gson().serialize(msgComp).replace("\\\\n", "\n");
         try {
             final net.minecraft.network.chat.Component comp = ComponentArgument.textComponent().parse(new StringReader(jsonComp));
